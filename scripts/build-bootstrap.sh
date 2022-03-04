734f31a1c583808bd87346880c17543ffe5657e5
Luke A. Guest <laguest@archeia.com>
2022-03-04 12:18:23 +0000

    Changes to get the script to work again since the new gcc:10 change of archived versions. Make all dot files use a gcc version number. Add GPRBuild to the bootstrap.

--- scripts/build-bootstrap.sh
+++ scripts/build-bootstrap.sh
@@ -26,34 +26,50 @@ function check_error()
 CPU_CORES="5"
 DISTFILES="/usr/portage/distfiles"
 
+GPRBUILD_GIT="https://github.com/AdaCore/gprbuild.git"
+XMLADA_GIT="https://github.com/AdaCore/xmlada.git"
+GPRCONFIG_KB_GIT="https://github.com/AdaCore/gprconfig_kb.git"
+
+# WARNING: Clean out your portage/distfiles directory as this can become cluttered.
+# TODO: Need a better way of matching up the patches archive.
 case ${GCC_MAJOR} in
 	9)
 		GCC_VER="9.4.0"
 		GCC_ARC="gcc-9.4.0.tar.xz"
-		GCC_PATCHES=$(ls ${DISTFILES}/gcc-9.4.0-patches*)
+		GCC_PATCHES_PATTERN="gcc-9.4.0-patches*"
+		# GCC_PATCHES=$(ls ${DISTFILES}/${GCC_PATCHES_PATTERN})
 		GCC_DIR=$(echo "${GCC_ARC}" | awk -F\. '{ print $1"."$2"."$3 }')
+		ADACORE_BRANCH="21.0"
 	;;
 
 	10)
-		GCC_VER="10.3.0"
-		GCC_ARC="gcc-10.3.0.tar.xz"
-		GCC_PATCHES=$(ls ${DISTFILES}/gcc-10.3.0-patches*)
-		GCC_DIR=$(echo "${GCC_ARC}" | awk -F\. '{ print $1"."$2"."$3 }')
+		GCC_VER="10"
+		GCC_ARC="gcc-10-20211126.tar.xz"
+		# GCC_ARC="gcc-10.3.0.tar.xz"
+		GCC_PATCHES_PATTERN="gcc-10.4.0-patches*"
+		# GCC_PATCHES=$(ls ${DISTFILES}/gcc-10.3.0-patches*)
+		# GCC_DIR=$(echo "${GCC_ARC}" | awk -F\. '{ print $1"."$2"."$3 }')
+		GCC_DIR=$(echo "${GCC_ARC}" | awk -F\. '{ print $1 }')
+		ADACORE_BRANCH="21.2"
 	;;
 
 	11)
-		echo "hello"
+		# echo "hello"
 		GCC_VER="11"
 		GCC_ARC="gcc-11-20220115.tar.xz"
-		GCC_PATCHES=$(ls ${DISTFILES}/gcc-11.3.0-patches*)
+		GCC_PATCHES_PATTERN="gcc-11.3.0-patches*"
+		# GCC_PATCHES=$(ls ${DISTFILES}/gcc-11.3.0-patches*)
 		GCC_DIR=$(echo "${GCC_ARC}" | awk -F\. '{ print $1 }')
-		echo ">> GCC_PATCHES: $GCC_PATCHES"
+		ADACORE_BRANCH="22.2"
+		# echo ">> GCC_PATCHES: $GCC_PATCHES"
 	;;
 
 	12)
 		GCC_VER="12"
 		GCC_ARC="gcc-11.3.0.tar.xz"
-		GCC_PATCHES=$(ls ${DISTFILES}/gcc-11.3.0-patches*)
+		GCC_PATCHES_PATTERN="gcc-11.3.0-patches*"
+		# GCC_PATCHES=$(ls ${DISTFILES}/gcc-11.3.0-patches*)
+		ADACORE_BRANCH=""
 	;;
 esac
 
@@ -61,31 +77,39 @@ if [[ ! -f ${DISTFILES}/${GCC_ARC} ]]; then
 	emerge -f sys-devel/gcc:${GCC_VER}
 fi
 
+GCC_PATCHES=$(ls ${DISTFILES}/${GCC_PATCHES_PATTERN})
+
 pushd /tmp
 
 # echo ${DISTFILES}/${GCC_ARC}
 # echo ${GCC_PATCHES}
 
-if [ ! -f .gcc-unpacked ]; then
+if [ ! -f .gcc-unpacked-${GCC_MAJOR} ]; then
+	echo ">> Unpacking ${GCC_ARC}..."
+
 	tar -xJpf ${DISTFILES}/${GCC_ARC}
 
-	check_error .gcc-unpacked
+	check_error .gcc-unpacked-${GCC_MAJOR}
 fi
 
-if [ ! -f .gcc-unpacked-patches ]; then
+if [ ! -f .gcc-unpacked-patches-${GCC_MAJOR} ]; then
+	echo ">> Unpacking ${GCC_PATCHES}..."
+
 	tar -xjpf ${GCC_PATCHES}
 
-	check_error .gcc-unpacked-patches
+	check_error .gcc-unpacked-patches-${GCC_MAJOR}
 fi
 
 cd ${GCC_DIR}
 
-if [ ! -f .gcc-patched ]; then
+if [ ! -f .gcc-patched-${GCC_MAJOR} ]; then
+	echo ">> Patching ${GCC_ARC}..."
+
 	for f in $(ls ../patch/[0-9]*_all_*.patch); do
 		patch -p1 < $f
 	done
 
-	check_error .gcc-patched
+	check_error .gcc-patched-${GCC_MAJOR}
 fi
 
 # TODO: Get base dir from archive.
@@ -100,7 +124,9 @@ cd ../${BUILD_DIR}
 
 ARCH="x86_64"
 
-if [ ! -f .gcc-configured ]; then
+echo ">> Configuring GCC..."
+
+if [ ! -f .gcc-configured-${GCC_MAJOR} ]; then
 	../${GCC_DIR}/configure \
 		--host=${ARCH}-pc-linux-gnu \
 		--build=${ARCH}-pc-linux-gnu \
@@ -135,25 +161,77 @@ if [ ! -f .gcc-configured ]; then
 		--without-isl \
 			&> log.configure.txt
 
-	check_error .gcc-configured
+	check_error .gcc-configured-${GCC_MAJOR}
 fi
 
-if [ ! -f .gcc-built ]; then
+if [ ! -f .gcc-built-${GCC_MAJOR} ]; then
+	echo ">> Building GCC..."
+
 	make -j${CPU_CORES} all &> log.build.txt
 
-	check_error .gcc-built
+	check_error .gcc-built-${GCC_MAJOR}
 fi
 
-if [ ! -f .gcc-installed ]; then
+if [ ! -f .gcc-installed-${GCC_MAJOR} ]; then
+	echo ">> Staging GCC..."
+
 	DESTDIR="/tmp" make install-strip &> log.build.install.txt
 
-	check_error .gcc-installed
+	check_error .gcc-installed-${GCC_MAJOR}
 fi
 
 cd /tmp
 
-if [ ! -f .gcc-archived ]; then
+## GPRBuild
+
+GPRBUILD_SRC_DIR="gprbuild-${GCC_MAJOR}"
+XMLADA_SRC_DIR="xmlada-${GCC_MAJOR}"
+GPRCONFIG_KB_SRC_DIR="gprconfig_kb-${GCC_MAJOR}"
+
+if [ ! -f .gprbuild-downloaded-${GCC_MAJOR} ]; then
+	git clone -b ${ADACORE_BRANCH} ${GPRBUILD_GIT} ${GPRBUILD_SRC_DIR}
+
+	check_error .gprbuild-downloaded-${GCC_MAJOR}
+fi
+
+if [ ! -f .xmlada-downloaded-${GCC_MAJOR} ]; then
+	git clone -b ${ADACORE_BRANCH} ${XMLADA_GIT} ${XMLADA_SRC_DIR}
+
+	check_error .xmlada-downloaded-${GCC_MAJOR}
+fi
+
+if [ ! -f .gprconfig_kb-downloaded-${GCC_MAJOR} ]; then
+	git clone -b ${ADACORE_BRANCH} ${GPRCONFIG_KB_GIT} ${GPRCONFIG_KB_SRC_DIR}
+
+	check_error .gprconfig_kb-downloaded-${GCC_MAJOR}
+fi
+
+if [ ! -f .gprbuild-built-${GCC_MAJOR} ]; then
+	cd ${GPRBUILD_SRC_DIR}
+
+	echo ">> Building / Staging GPRBuild..."
+
+	DESTDIR=/tmp ./bootstrap.sh --with-xmlada=../${XMLADA_SRC_DIR} --with-kb=../${GPRCONFIG_KB_SRC_DIR} --prefix=/opt/${INSTALL_DIR} &> log.build.txt
+
+	check_error .gprbuild-built-${GCC_MAJOR}
+fi
+
+## Installation
+
+cd /tmp
+
+echo ">> Archiving..."
+
+if [ ! -f .gcc-archived-${GCC_MAJOR} ]; then
 	tar -cJpf ${INSTALL_DIR}-${ARCH}.tar.xz -C ./opt ${INSTALL_DIR}
 
-	check_error .gcc-archived
+	check_error .gcc-archived-${GCC_MAJOR}
 fi
+
+## Clean up
+
+echo ">> Cleaning up..."
+
+rm -rf ./opt ./${BUILD_DIR} ./${GCC_DIR} ./patch ${GPRBUILD_SRC_DIR} ${GPRCONFIG_KB_SRC_DIR} ${XMLADA_SRC_DIR} .gpr* .gcc* .xmlada*
+
+popd
