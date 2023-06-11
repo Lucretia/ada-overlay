708298fa069093bea14eaaabd722bf961f02013f
Luke A. Guest <laguest@archeia.com>
2023-06-11 10:58:59 +0100

    Add code to commit the manifest.

--- scripts/create-new-manifest.sh
+++ scripts/create-new-manifest.sh
@@ -9,4 +9,13 @@ rm Manifest
 ebuild $(ls *.ebuild|head -n1) manifest
 popd
 
-git status
+# There should only be one file.
+FILE=$(git status | grep -w modified | grep -w Manifest | awk '{ print $2 }')
+
+if [ -z ${FILE} ]; then
+    printf ">> ${RED}None Found${NC}.\n"
+else
+    echo ${FILE}
+
+    git commit -m "Updated sys-devel/gcc/Manifest." -v ${FILE}
+fi
